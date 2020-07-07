97f6e922dc36ee66faf250b666e4df5344181d68
Victor <victorbesse6@gmail.com>
2020-07-08 00:19:08 +0200

    creation route sauvegarde de l'image  dans le datastet de progressive learning renvoyee par l'utilisateur (corrigee ou non)

--- index.py
+++ index.py
@@ -3,7 +3,7 @@ import tornado.ioloop
 from call_model import *
 import io
 from PIL import Image
-
+import uuid
 
 class ImgClassifierHandler(tornado.web.RequestHandler):
     def post(self):
@@ -40,33 +40,72 @@ class ImgClassifierHandler(tornado.web.RequestHandler):
         result = model_predict(model, processed_img)
         self.write(f"{result}")
 
-
 class IndexHandler(tornado.web.RequestHandler):
     def get(self):
         self.render('index.html')
 
 
-class DatasetCompletionHandler(tornado.web.RequestHandler):
-    pass
+class VsdHandler(tornado.web.RequestHandler):
+    def post(self):
+        valid_ext = ['JPG', 'JPEG', 'PNG']
 
+        try:
+            file1 = self.request.files["fileImage"][0]
+            value = self.get_argument('model_class')
+            file_name = file1['filename']
+            file_ext = os.path.splitext(file_name)[1]
+            file_body = file1['body']
+
+        except Exception as e:
+            print(e)
+            self.write(f"POST request empty!")
+            self.finish()
+            return
+
+        file_ext = file_ext.strip().upper().replace(".", "")
+
+        if file_ext not in valid_ext:
+            self.write(f"only jpg, jpeg and png are ok")
+            self.finish()
+            return
+
+        try:
+            img = io.BytesIO(file_body)
+            img = Image.open(img)
+        except Exception as e:
+            print(e)
+            self.write(f"Unable to read file!")
+            self.finish()
+            return
+
+        fname = str(uuid.uuid4())+"."+file_ext.lower()
+        image_folder = os.path.join(SAMPLES_DATASET, str(value), fname)
+
+        try:
+            img.save(image_folder)
+        except:
+            self.write(f"Unable to keep file!")
+            self.finish()
+
+        self.write(f"thank you for your return !")
 
 if __name__ == "__main__":
     ROOT_PATH = os.getcwd()
     IMAGE_PATH = "image"
-    NEW_SAMPLES_DATASET = "new_sample_dataset"
+    SAMPLES_DATASET = "samples_dataset"
     MODEL_FOLDER = "models"
 
     if not os.path.exists(IMAGE_PATH):
         os.makedirs(IMAGE_PATH)
 
-    if not os.path.exists(NEW_SAMPLES_DATASET):
-        os.makedirs(NEW_SAMPLES_DATASET)
+    if not os.path.exists(SAMPLES_DATASET):
+        os.makedirs(SAMPLES_DATASET)
 
     for i in range(5):
         a = i + 1
         a = str(a)
-        if not os.path.exists((os.path.join(NEW_SAMPLES_DATASET, a))):
-            os.mkdir(os.path.join(NEW_SAMPLES_DATASET, a))
+        if not os.path.exists((os.path.join(SAMPLES_DATASET, a))):
+            os.mkdir(os.path.join(SAMPLES_DATASET, a))
 
     model_path = os.path.join(ROOT_PATH,MODEL_FOLDER)
     model_name = "best_CNN_Conv32_MaxPool2_Conv64_MaxPool2_Conv32_MaxPool2_Dense64relu_Dropout05_Dense32Relu_Dropout05_Dense6Relu.h5"
@@ -74,10 +113,11 @@ if __name__ == "__main__":
 
     app = tornado.web.Application([
         (r"/image", ImgClassifierHandler),
-        (r"/new_sample_dataset", DatasetCompletionHandler),
+        (r"/uvsd", VsdHandler), # user  validated sample dataset / response validated by user
         (r"/", IndexHandler)
     ])
     server = tornado.httpserver.HTTPServer(app, max_buffer_size=200000)  # 2Mo
     server.listen(5050)
     print("listening on port 5050")
     tornado.ioloop.IOLoop.current().start()
+
